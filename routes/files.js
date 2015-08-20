9cb542aa357469c174b03a9b1c0b6d6084e78a92
Baptiste <baptiste.g44@gmail.com>
2015-08-20 17:07:21 +0200

    first working download page
    get availables files from directory
    jade template with clic to download

--- routes/files.js
+++ routes/files.js
@@ -1,9 +1,23 @@
 var express = require('express');
 var router = express.Router();
+var fs = require('fs');
+var path = require('path');
+
+var filesLocation = "../public/files";
 
 /* GET available files. */
 router.get('/', function(req, res, next) {
-  res.send('file list');
+	console.log(__dirname);
+	console.log(path.join(__dirname, filesLocation));
+	fs.readdir(path.join(__dirname, filesLocation), function(err, files) {
+		if (err) {
+			throw err;
+		}
+		res.render('files', {
+			title: 'Available files',
+			files: files
+		});
+	});
 });
 
 module.exports = router;
